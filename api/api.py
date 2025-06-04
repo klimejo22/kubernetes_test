66e32af9f2c14f05fb4702f4711fcb03913b41b8
josefkliment <klimejo22@sps-prosek.cz>
2025-06-04 10:50:39 +0200

    Pridani redisu

--- api/api.py
+++ api/api.py
@@ -1,7 +1,22 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+import redis
 
+redis_client = redis.Redis(host="redis", port=6379, db=0)
 app = FastAPI()
 
+@app.middleware("http")
+async def count_requests(
+    request: Request,
+    call_next
+):
+    redis_client.incr("totalRequests")
+    response = await call_next(request)
+    return response
+    
 @app.get("/helloWorld")
 def helloWorld():
     return {"Hello": "World"}
+
+@app.get("/stats")
+def stats():
+    return {"Total requests": redis_client.get("totalRequests")}
